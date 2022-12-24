2a4d004810224f740d9e06014f9f3fad3f53c724
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2022-12-24 13:52:19 -0500

    Pretty much done, nice short project.
    Possible improvements:
    Add support for backward 01 format instead of forward HV format.
    Reposition the input boxes.
    Validate whether its a real taxi walk.
    Show Manhattan arrows.

--- functions.js
+++ functions.js
@@ -1,38 +1,114 @@
-function fillGraph(size) {
+const colors = ["red", "blue", "green", "purple", "orange"];
+
+function draw() {
+    let size = parseInt(document.getElementById('size').value);
     let graph = document.getElementById("graph");
+
+    // Clear the graph and resize it to the current size.
     removeAllChildNodes(graph)
-    let numCells = 2 * size;
-    graph.style.gridTemplateRows = "repeat(" + numCells.toFixed() + "," + (700 / numCells).toFixed() + "px)";
-    graph.style.gridTemplateColumns = "repeat(" + numCells.toFixed() + "," + (700 / numCells).toFixed() + "px)";
+    let width = 2 * size;
+    graph.style.gridTemplateRows = "repeat(" + width.toFixed() + "," + (700 / width).toFixed() + "px)";
+    graph.style.gridTemplateColumns = "repeat(" + width.toFixed() + "," + (700 / width).toFixed() + "px)";
 
+
+    // Loop over every cell in the graph and create a div with 4 black borders.
+    // size + 1 for the ghost bubbles whose top and left borders form the bottom and right borders of the graph.
     let lineWeight = (30 / size).toFixed();
-    // size + 2 for the ghost bubbles whose top and left borders form the bottom and right borders of the graph.
-    for(let r = -size + 1; r < size + 2; r++) {
-        for(let c = -size + 1; c < size + 2; c++) {
+    for(let r = -size; r < size + 1; r++) {
+        for(let c = -size; c < size + 1; c++) {
             var div = document.createElement("div");
-            div.id = r.toFixed() + "," + c.toFixed();
-            div.className = "box";
-            div.textContent = " "
+            div.id = (-r).toFixed() + "," + c.toFixed();
 
-            div.style.gridRowStart = r + size;
-            div.style.gridRowEnd = (r + size + 1);
-            div.style.gridColumnStart = c + size;
-            div.style.gridColumnEnd = (c + size + 1);
+            div.style.gridRowStart = r + size + 1;
+            div.style.gridRowEnd = r + size + 2;
+            div.style.gridColumnStart = c + size + 1;
+            div.style.gridColumnEnd = c + size + 2;
 
-            if(r < size + 1) {
-                div.style.borderLeft = lineWeight + "px solid black";
+            if(r < size) {
+                if(c == 0) {
+                    // y-axis.
+                    div.style.borderLeft = lineWeight + "px solid black";
+                }
+                else {
+                    // Dotted non-axis lines.
+                    div.style.borderLeft = 1 + "px dotted black";
+                }
             }
-            if(c < size + 1) {
-                div.style.borderTop = lineWeight + "px solid black";
+            if(c < size) {
+                if(r == 0) {
+                    div.style.borderTop = lineWeight + "px solid black";
+                }
+                else {
+                    div.style.borderTop = 1 + "px dotted black";
+                }
             }
             graph.appendChild(div);
         }
     }
-    document.getElementById("1,1").style.borderTop = lineWeight + "px solid red";
+
+    for(let i = 1; i <= 5; i++) {
+        let walk = document.getElementById('walk' + i.toFixed()).value;
+        drawWalk(walk, colors[i - 1], size);
+    }
 }
 
+// For each step, change the corresponding border to red.
+function drawWalk(walk, color, size) {
+    let x = 0;
+    let y = 0;
+    let xStep = 1;
+    let yStep = 1;
+    for(const char of walk) {
+        if(char == "H") {
+            let newX = x + xStep;
+            drawStep(x, y, newX, y, color, size);
+            x = newX;
+            yStep = -yStep;
+        }
+        else if(char == "V") {
+            let newY = y + yStep;
+            drawStep(x, y, x, newY, color, size);
+            y = newY;
+            xStep = -xStep;
+        }
+    }
+}
+
+// Clears the graph.
 function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
+    while(parent.firstChild) {
         parent.removeChild(parent.firstChild);
+    }
+}
+
+function drawStep(x1, y1, x2, y2, color, size) {
+    let r = 0;
+    let c = 0;
+    let borderLocation = "";
+    if(x2 == x1 + 1) {
+        r = y1;
+        c = x1;
+        borderLocation = "borderTop";
+    }
+    else if(x2 == x1 - 1) {
+        r = y1;
+        c = x2;
+        borderLocation = "borderTop";
+    }
+    else if(y2 == y1 + 1) {
+        r = y1 + 1;
+        c = x1;
+        borderLocation = "borderLeft";
+    }
+    else {
+        r = y2 + 1;
+        c = x1;
+        borderLocation = "borderLeft";
+    }
+
+    if(r > -size && r <= size && c >= -size && c < size) {
+        let div = document.getElementById(r.toFixed() + "," + c.toFixed());
+        let border = (50 / size).toFixed() + "px solid " + color;
+        div.style[borderLocation] = border;
     }
 }
