cf2d6c0f1160537f78b4fbd5c6c4eef84fbbca6d
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2022-12-24 02:41:20 -0500

    Fixed bottom and right border.
    Now there's one white pixel in the bottom right.

--- functions.js
+++ functions.js
@@ -6,8 +6,9 @@ function fillGraph(size) {
     graph.style.gridTemplateColumns = "repeat(" + numCells.toFixed() + "," + (700 / numCells).toFixed() + "px)";
 
     let lineWeight = (30 / size).toFixed();
-    for(let r = -size + 1; r < size + 1; r++) {
-        for(let c = -size + 1; c < size + 1; c++) {
+    // size + 2 for the ghost bubbles whose top and left borders form the bottom and right borders of the graph.
+    for(let r = -size + 1; r < size + 2; r++) {
+        for(let c = -size + 1; c < size + 2; c++) {
             var div = document.createElement("div");
             div.id = r.toFixed() + "," + c.toFixed();
             div.className = "box";
@@ -18,14 +19,11 @@ function fillGraph(size) {
             div.style.gridColumnStart = c + size;
             div.style.gridColumnEnd = (c + size + 1);
 
-            div.style.borderTop = lineWeight + "px solid black";
-            div.style.borderLeft = lineWeight + "px solid black";
-
-            if(r == size) {
-                div.style.borderBottom = lineWeight + "px solid black";
+            if(r < size + 1) {
+                div.style.borderLeft = lineWeight + "px solid black";
             }
-            if(c == size) {
-                div.style.borderRight = lineWeight + "px solid black";
+            if(c < size + 1) {
+                div.style.borderTop = lineWeight + "px solid black";
             }
             graph.appendChild(div);
         }
